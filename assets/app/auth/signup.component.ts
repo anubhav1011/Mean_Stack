c00023e239f024aa44f2003cefdf5f662f0c701c
anubhav1011 <gupta.anu@husky.neu.edu>
2018-03-09 16:17:56 -0500

    Adding user signup with reactive forms

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,4 +1,5 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
 
@@ -6,7 +7,27 @@ import {Component} from "@angular/core";
     templateUrl: './signup.component.html'
 })
 
-export class SignupComponent {
+export class SignupComponent implements OnInit {
 
 
+    myForm: FormGroup;
+
+    onSubmit() {
+        console.log(this.myForm);
+        this.myForm.reset();
+    }
+
+    ngOnInit() {
+        this.myForm = new FormGroup({
+
+            firstName: new FormControl(null, Validators.required),
+            lastName: new FormControl(null, Validators.required),
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.email
+            ]),
+            password: new FormControl(null, Validators.required),
+
+        });
+    }
 }
