b08a491f4671699b910b24b90d221aea75080018
anubhav1011 <gupta.anu@husky.neu.edu>
2018-03-08 19:12:44 -0500

    Adding new component along with model with two way binding

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,32 +1,13 @@
 import {Component} from '@angular/core';
+import {Message, Message} from "./messages/message.model";
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styles: [`
-    
-        .author{
-            display: inline-block;
-            font-style: italic;
-            font-size: 12px;
-            width: 80%
-            
-        }
-        .config{
-            display: inline-block;
-            text-align: right;
-            font-size: 12px;
-            width: 19%;
-            
-        }
-    
-    `]
+    styles: []
 })
 export class AppComponent {
 
-    message = {
+    message: Message = new Message('Some message', 'Anubhav');
 
-        content: 'A message',
-        author: 'Anubhav'
-    };
 }
