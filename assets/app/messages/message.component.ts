b4115c179576c12a002e72a0bd52ab00fe2924c2
anubhav1011 <gupta.anu@husky.neu.edu>
2018-03-09 16:28:56 -0500

    Adding delete functionality

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,11 +1,12 @@
 import {Component, EventEmitter, Input, Output} from "@angular/core";
 import {Message} from "./message.model";
+import {MessageService} from "./message.service";
 
 
 @Component({
     selector: 'app-message',
     templateUrl: './message.component.html',
-    styles:[`
+    styles: [`
 
         .author {
             display: inline-block;
@@ -34,11 +35,20 @@ export class MessageComponent {
     @Input() message: Message;
     @Output() editClicked = new EventEmitter<string>();
 
+    constructor(private messageService: MessageService) {
+
+
+    }
+
     color = 'red';
 
     onEdit() {
 
         this.editClicked.emit('A new value');
+    }
+
+    onDelete() {
+        this.messageService.deleteMessage(this.message);
     }
 
 
